7cf17171fc561fab6a990defbcd381292183bc41
endre88 <endre1988@freemail.hu>
2024-01-26 12:23:19 +0100

    váltoáskövetés

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import './scss/style.scss';
 import config from './db_config.js';
-import {initializeApp} from 'firebase/app'
-import {getFirestore, collection, addDoc, Timestamp, query, orderBy,getDocs} from 'firebase/firestore'
+import {initializeApp} from 'firebase/app';
+import {getFirestore, collection, addDoc, Timestamp, query, orderBy,getDocs, onSnapshot} from 'firebase/firestore'
+import scrollIntoView from 'scroll-into-view-if-needed';
 
 const app= initializeApp(config)
 
@@ -9,22 +10,22 @@ const db=getFirestore(app)
 
 async function sendMessage(message){
 const docRef=await addDoc(collection(db,"messages"),message); //read data firebase
-console.log('Document written witht ID: ', docRef.id)
+//console.log('Document written witht ID: ', docRef.id)
 }
 
 function createMessage(){
     const message=document.querySelector('#message').value;
     const username= document.querySelector('#nickname').value; 
-    const date=Timestamp.now();
-    return {message:message,username,date};
+    const date=Timestamp.now().getHours();
+    return {message,username,date}; 
 }
 
 /** dowloads all messages from databes and display them ordereb by date*/
 async function displayAllMessages(){
-  const q=query(collection(db,'messages'), orderBy('date', 'desc'));
-const messages= await getDocs(q);
-document.querySelector('#messages').innerHTML='';
-messages.forEach((doc)=>{
+  const q=query(collection(db,'messages'), orderBy('date', 'asc'));
+  const messages= await getDocs(q);
+  document.querySelector('#messages').innerHTML='';
+  messages.forEach((doc)=>{
   displayMessage(doc.data());
 })
 }
@@ -36,7 +37,7 @@ function displayMessage(message){
             <i class="fas fa-user"></i>
             <div>
               <span class="username">${message.username}
-                <time>just now</time>
+                <time>${message.date}</time>
               </span>
               <br />
               <span class="message-text">${message.message}</span>
@@ -48,13 +49,38 @@ function displayMessage(message){
           </div>
           `;
       document.querySelector('#messages').insertAdjacentHTML('beforeend', messageHTML);
+      scrollIntoView(document.querySelector('#messages'),{
+        scrollMode:'if-needed',
+        block:'end'
+      })
 }
 
+function handleSubmit(){
+  const message=createMessage();
+  if (message.message&&message.username) {
+  sendMessage(message);
+}
+}
 
+document.querySelector('#send').addEventListener('click', handleSubmit);
 
-document.querySelector('#send').addEventListener('click',()=>{
-    const message=createMessage();
-    if (message.message&&message.username) sendMessage(message);
+document.addEventListener('keyup', (event)=>{
+  if (event.key==='Enter'){
+    handleSubmit();
+  }
+})
+
+onSnapshot(collection(db,'messages'),(snapshot)=>{
+  snapshot.docChanges().forEach((change)=>{
+    if (change.type ==='added'){
+      displayMessage(change.doc.data());}
+    if (change.type ==='modified'){
+      console.log('új Modified');
+    }
+    if (change.type ==='removed'){
+      console.log('Removed'); 
+    }
+  })
 })
 
 window.addEventListener('DOMContentLoaded',()=> { displayAllMessages();})
